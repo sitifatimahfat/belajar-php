32beaf40b139235016f3d37d9fb5b8e24830b79b
Siti Fatimah <siti.fatimah2020@unida.ac.id>
2022-10-24 11:06:30 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Siti Fatimah";
+$umur = 21;
+
+echo "Nama saya <strong> $nama </strong>,saya berusia $umur tahun.<br>";
+
+$namaAdik= "Hanna";
+$umurAdik = 16;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun <br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun "
+
+
+?>
